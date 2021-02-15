5e3ce0aa04c145048267d95901a96e3d8a7474a2
Angel Octavio Lopez <a.o.lopez.cruz@gmail.com>
2021-02-15 02:53:36 -0600

    primer version maqueeta podcast

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,166 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <header class="cover">
+      <div class="container">
+        <nav
+          class="navbar is-transparent"
+          role="navigation"
+          aria-label="main navigation"
+        >
+          <div class="navbar-brand">
+            <a class="navbar-item" href="/">
+              <img src="./assets/logo.svg" width="112" height="28" />
+            </a>
+            <a
+              role="button"
+              class="navbar-burger"
+              aria-label="menu"
+              aria-expanded="false"
+              data-target="navbarBasicExample"
+            >
+              <span aria-hidden="true"></span>
+              <span aria-hidden="true"></span>
+              <span aria-hidden="true"></span>
+            </a>
+          </div>
+          <div id="navbarBasicExample" class="navbar-menu">
+            <div class="navbar-end">
+              <a class="navbar-item"> Seccion 1 </a>
+              <a class="navbar-item"> Seccion 2 </a>
+              <a class="navbar-item"> Seccion 3 </a>
+            </div>
+          </div>
+        </nav>
+      </div>
+      <div class="container">
+        <div class="mt-6 content-header">
+          <h1 class="is-size-1 px-3 title-podcast">PODCAST</h1>
+          <p class="mt-5 px-3 is-size-3 has-text-white">
+            Unde quia minima aspernatur quam fugit in est. Autem et ipsam
+            molestias ad autem ut aut. Et alias eveniet occaecati et.
+          </p>
+          <button class="mx-3 my-6 px-6 button is-rounded btn">
+            Capítulos
+          </button>
+        </div>
+      </div>
+    </header>
+    <main>
+      <section class="intro-episodio">
+        <div class="container">
+          <div class="columns pt-6">
+            <div class="column">
+              <img src="./assets/CardPodcast.svg" alt="podcast">
+            </div>
+            <div class="column">
+              <h2 class="is-size-1">Nihil cupiditate illo</h2>
+              <p class="is-size-4 mt-3">
+                Tempore qui mollitia consequatur quia. Eaque ut et autem facere.
+                Debitis quis et ut quisquam praesentium dolorum aut.
+              </p>
+              <div>
+                <button class="mx-3 my-6 px-6 button is-rounded btn2">Escucha ahora</button>
+                <button class="mx-3 my-6 px-6 button is-rounded btn2">Lista de Capitulos</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section class="section-medium">
+        <div class="container">
+          <div class="columns">
+            <div class="column pt-6 bg-medium">
+              <h1 class="is-size-1">Natus asperiores illo</h1>
+              <p class="is-size-4 mt-3">Asperiores consequatur sint quam omnis sint. Molestias quibusdam quaerat et vel quas. Magnam qui velit blanditiis. Incidunt dolorem veritatis aut consequatur nihil nemo ab. Sunt at omnis non eum similique voluptas qui.</p>
+            </div>
+            <div class="column is-flex is-justify-content-center">
+              <img src="./assets/Podcast-color.svg" alt="svg" class="imagen">
+            </div>
+          </div>
+        </div>
+      </section>
+    </main>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body {
+  background-color: #474d8a;
 }
+
+.cover {
+  width: 100vw;
+  height: 100vh;
+  background-image: url("assets/podcastlanding.svg");
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-clip: border-box;
+  background-position: -0.5rem;
+}
+
+.navbar {
+  background-color: transparent;
+}
+
+.navbar-item {
+  color: white;
+}
+
+.navbar-item:hover {
+  color: #ef7a23 !important;
+}
+
+.title-podcast {
+  color: #ef7a23;
+}
+
+.btn, .btn2 {
+  background-color: #ef7a23;
+  color: #fff;
+  border: none;
+}
+
+.btn:hover {
+  background-color: #3e99d5;
+  color: #fff;
+}
+
+.btn2:hover {
+  background-color: #1e3c91;
+  color: #fff;
+}
+
+.content-header {
+  width: 50%;
+}
+
+.intro-episodio {
+  height: 32rem;
+  color: #fff;
+  background-image: url('assets/fondo2.svg');
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-clip: border-box;
+}
+
+.section-medium {
+  height: 32rem;
+  color: #fff;
+  overflow: hidden;
+}
+
+.imagen {
+  width: 30rem;
+}
+
+.bg-medium {
+  background-image: url('assets/fondo3.svg');
+}
+
 </style>
